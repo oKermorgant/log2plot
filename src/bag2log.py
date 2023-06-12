ae159a0c8d48a9b01ec6a2d8958ee3e54db25979
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2023-06-13 01:35:28 +0200

    actually no legend when asked not to

---
+++ src/bag2log.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import rosbag2_py as rb
+from rclpy.serialization import deserialize_message
+import os
+
+
+class Bag2log:
+    def __init__(self, bag):
+
+        storage = rb.StorageOptions(bag)
+        conv = rb.ConverterOptions('cdr','cdr')
+        self.reader = rb.SequentialReader()
+        self.reader.open(storage, conv)
+
+        self.topics = [topic.name for topic in self.reader.get_all_topics_and_types()]
+
